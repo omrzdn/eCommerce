954006faec97eb9cb34f9f7124b46478e67879bc
omda <emadsoso002@gmail.com>
2019-11-22 21:34:32 +0200

    edit by emad

--- Admin/layout/js/backend.js
+++ Admin/layout/js/backend.js
@@ -1,18 +1,18 @@
 $(document).ready(function(){
-    
+
    'use strict';
-    
+
     $('[placeholder]').focusin(function(){
-        
-       
-        // $(this).attr('data-text', $(this).attr('placeholder'));
-        $(this).dataset("text") = $(this).attr('placeholder');
+
+
+        $(this).attr('data-text', $(this).attr('placeholder'));
+        //$(this).dataset("text") = $(this).attr('placeholder');
         $(this).attr('placeholder', '');
-        
-        
+
+
     }).focusout(function (){
-        
+
         $(this).attr('placeholder', $(this).attr('data-text'));
     });
-    
+
 });
