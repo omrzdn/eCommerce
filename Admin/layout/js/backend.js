9f885105037c5956d4b2e062d105d1247ab60932
omda <emadsoso002@gmail.com>
2019-11-22 21:38:38 +0200

    edits by alshennawi

--- Admin/layout/js/backend.js
+++ Admin/layout/js/backend.js
@@ -2,11 +2,11 @@ $(document).ready(function(){
 
    'use strict';
 
-    $('[placeholder]').focusin(function(){
+    //$('[placeholder]').focusin(function(){
 
 
-        $(this).attr('data-text', $(this).attr('placeholder'));
-        //$(this).dataset("text") = $(this).attr('placeholder');
+        //$(this).attr('data-text', $(this).attr('placeholder'));
+        $(this).dataset("text") = $(this).attr('placeholder');
         $(this).attr('placeholder', '');
 
 
